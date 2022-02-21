b9f2ebd8a76a8ed46824bcf5e2af295bb71d744d
kmh <dkakqlql@gmail.com>
2022-02-21 20:12:29 +0900

    Test Commit

---
+++ app/src/main/java/com/stn/cocoatalk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stn.cocoatalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
